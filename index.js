e82d0795228aad22a37f899eeb4ad2991fb4561a
JOSEPH KIM <joseph@owllabs.co>
2018-04-11 00:09:25 -0700

    http2 request

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const spdy = require('spdy')
+const fs = require('fs')
 const app = express();
 const path = require("path");
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
 app.use('/static', express.static(path.join(__dirname, 'public')));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const options = {
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert:  fs.readFileSync(__dirname + '/server.crt')
+}
+spdy
+  .createServer(options, app)
+  .listen(3000, (error) => {
+    if (error) {
+      console.error(error)
+      return process.exit(1)
+    } else {
+        console.log('Example app listening on port 3000!');
+    }
+  })
