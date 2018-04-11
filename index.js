805473060e1e2a52786e291922d5c966b58b614d
JOSEPH KIM <josephkim.dev@gmail.com>
2018-04-10 17:57:10 -0700

    image_split_sample

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const path = require("path");
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
-
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
